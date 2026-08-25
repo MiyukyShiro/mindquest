19c0e4400f3505ef3d35a73fc567f00a8d0e0eba
MiyukyShiro <tschui.silvan@protonmail.com>
2026-08-25 22:43:33 +0200

    Add files via upload

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,7 @@
 /* Lumo Service Worker
    WICHTIG: index.html wird zuerst aus dem Netz geholt, damit eine neue Fassung
    sofort ankommt. Der Cache ist nur der Rückfall, wenn du offline bist. */
-const CACHE = "lumo-v8";
+const CACHE = "lumo-v20";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png",
   "./apple-touch-icon.png", "./apple-touch-icon-180.png"];
 
